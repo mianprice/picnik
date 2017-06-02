ed09816399c2fef34a32f9305cb8d2defd62e8de
aaroniousblanco <aaronawhite1@gmail.com>
2017-06-02 10:49:38 -0400

    Update home page styling.

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as ReactRedux from 'react-redux';
 import * as actions from './Home.actions';
 import picnik_background from '../../public/picnik-color.jpg';
+import {hashHistory} from 'react-router';
 
 class Home extends React.Component {
   render() {
@@ -15,8 +16,8 @@ class Home extends React.Component {
     };
     return (
         <div className="home" style={home_style}>
-            {/*<div className="home-buttons">Sign Up</div>
-            <div className="home-buttons">Plan Your Picnik Now</div>*/}
+            <div className="home-buttons-get-started" onClick={() => hashHistory.push('/signup')}>Personalize Your Experience</div>
+            <div className="home-buttons-plan-now" onClick={() => hashHistory.push('/planning')}>Plan Your Picnik Now</div>
             <div className="welcome-section">
                 <div>Here at picnik, we give you the tools to create a perfect day out at the park. <br/><br/>We handle everything from helping you pick the perfect park, the perfect day, and intelligent food and drink pairings based on the things you like. <br/><br/>You can even invite your friends to join you on your day out!</div>
             </div>
