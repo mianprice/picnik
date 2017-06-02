a8ec82e2effdd71192f9e2503f09d18d39ed4d3d
Ian Price <michael.ian.price@gmail.com>
2017-06-02 13:11:13 -0400

    signup styling

--- frontend/src/signup/Signup.js
+++ frontend/src/signup/Signup.js
@@ -77,62 +77,60 @@ class Signup extends React.Component {
               <div className="wine_section">
 
                 <div className="wine_instructions">
-                Here we suggest keeping your options open. Sometimes a properly paired wine 			can really change your mind, but if you really don’t like Reds, or can’t stand Sparkling 		wine, we won’t judge, Simply select the wines you don’t want us to show you.
+                    "Here we suggest keeping your options open. Sometimes a properly paired wine can really change your mind, but if you really don’t like Reds, or can’t stand Sparkling wine, we won’t judge, Simply select the wines you DON'T want us to show you."
                 </div>
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.dry_whites === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('dry_whites')}>Dry Whites</div>
+                  <div className={this.props.signup.wine_profile.dry_whites === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('dry_whites')}>Dry Whites</div>
                   <div className="wine_category_description">Dry, crisp, sometimes tart and usually refreshing, these are among the lightest wines. <br/>
                   ex. Sauvignon Blanc, Albariño, Pinot Grigio, ...
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.sweet_whites === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('sweet_whites')}>Sweet Whites</div>
+                  <div className={this.props.signup.wine_profile.sweet_whites === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('sweet_whites')}>Sweet Whites</div>
                   <div className="wine_category_description">
-                  Sweet while sweet, these wines are also generally balanced. The natural sugars from the grapes help to balance the higher than average acidity or bitterness present in these wines.
-                  examples: Chenin Blanc, Riesling , Torrontés
+                  While sweet, these wines are also generally balanced. The natural sugars from the grapes help to balance the higher than average acidity or bitterness present in these wines. </br>
+                  examples: Chenin Blanc, Riesling , Torrontés, …
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.rich_whites === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('rich_whites')}>Rich Whites</div>
+                  <div className={this.props.signup.wine_profile.rich_whites === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('rich_whites')}>Rich Whites</div>
                   <div className="wine_category_description">
-                  Rich Whites - fuller in body and sometimes oaky, these wines tend to be a bit heavier on the pallet than their Dry counterparts
-                  ex. Chardonnay, Sémillon, Viogner, ...
+                    Fuller in body and sometimes oaky, these wines tend to be a bit heavier on the palate than their Dry counterparts. </br>
+                    ex. Chardonnay, Sémillon, Viogner, …
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.light_reds === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('light_reds')}>Light Reds</div>
+                  <div className={this.props.signup.wine_profile.light_reds === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('light_reds')}>Light Reds</div>
                   <div className="wine_category_description">
-                  Light Reds - Subtle and lighter in body compared to other red wines, lower tannins and bright acidity make these wines very drinkable
-                  ex. Pinot Noir, Gamay, Brachetto
+                    Subtle and lighter in body compared to other red wines, lower tannins and bright acidity make these wines very drinkable. </br>
+                    ex. Pinot Noir, Gamay, Brachetto, …
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.medium_reds === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('medium_reds')}>Medium Reds</div>
+                  <div className={this.props.signup.wine_profile.medium_reds === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('medium_reds')}>Medium Reds</div>
                   <div className="wine_category_description">
-                  Medium Reds - medium in body with a broad spectrum of flavor profiles, these are some of the most food friendly wines
-                  ex. Merlot, Sangiovese, Barbera, …
+                    Medium in body with a broad spectrum of flavor profiles, these are some of the most food friendly wines. </br>
+                    ex. Merlot, Sangiovese, Barbera, …
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.bold_reds === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('bold_reds')}>Bold Reds</div>
+                  <div className={this.props.signup.wine_profile.bold_reds === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('bold_reds')}>Bold Reds</div>
                   <div className="wine_category_description">
-                  Bold Reds - High in tannins and bold in flavor, these wines can hold their own when paired with very flavorful dishes
-                  ex. Cabernet Sauvignon, Malbec, Syrah, …
+                    High in tannins and bold in flavor, these wines can hold their own when paired with very flavorful dishes. </br>
+                    ex. Cabernet Sauvignon, Malbec, Syrah, …
                   </div>
                 </div>
 
                 <div className="wine_category">
-                  <div className={this.props.signup.wine_profile.sparkling === false ? " wine_button wine_button_selected" : "wine_button"} onClick={() => this.props.setWine('sparkling')}>Sparkling</div>
-                  <div>
-                  Sparkling
-                  Ranging from dry to sweet, sparkling wines are good for a variety of occasions and cuisines.
-
-                  ex. cava, champagne, prosecco, …
+                  <div className={this.props.signup.wine_profile.sparkling === false ? " signup_button_selected" : "signup_button"} onClick={() => this.props.setWine('sparkling')}>Sparkling</div>
+                  <div className="wine_category_description">
+                    Ranging from dry to sweet, sparkling wines are good for a variety of occasions and cuisines. </br>
+                    ex. Cava, Champagne, Prosecco, …
                   </div>
                 </div>
               </div>
@@ -144,8 +142,11 @@ class Signup extends React.Component {
           (
             <div className="signup_section">
               <div className="beer-signup-section-container">
+              <div className="wine_instructions">
+                "Same as with wine, we suggest keeping an open mind, especially if you’ve never heard of or tasted a particular type of beer. Let us pair it with a dish that’ll really wow you. If you just can’t stand a hoppy beer, or if stouts are not your thing, that’s perfectly ok. Simply select the beers you DON'T want us to show you."
+              </div>
                   {beer_profile_array.map((item, index) =>
-                      <div key={index} className={this.props.signup.beer_profile[item.toLowerCase()] === false ? "signup_button_selected" : "signup_button"} onClick={() => this.props.setBeer(item.toLowerCase())}>{item}</div>
+                        <div key={index} className={this.props.signup.beer_profile[item.toLowerCase()] === false ? "signup_button_selected" : "signup_button"} onClick={() => this.props.setBeer(item.toLowerCase())}>{item}</div>
                   )}
                   {/* CAN'T MAP OVER THE BEER BELOW BECAUSE ITS NAME IN THE STATE IS DIFFERENT THAN IT APPEARS IN THE VIEW */}
                 <div className={this.props.signup.beer_profile.pale_ale === false ? "signup_button_selected" : "signup_button"} onClick={() => this.props.setBeer('pale_ale')}>Pale Ale</div>
