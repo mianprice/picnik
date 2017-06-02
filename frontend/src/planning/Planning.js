87f0debaae4f83b3e7785ec84b0c41e71c013bb8
Ian Price <michael.ian.price@gmail.com>
2017-06-02 12:08:07 -0400

    more redirect

--- frontend/src/planning/Planning.js
+++ frontend/src/planning/Planning.js
@@ -94,9 +94,9 @@ class Planning extends React.Component {
                         </div>
                         <div className="save-picnik-buttons-container">
                             <div className="save-picnik-button">
-                                <Link className="plan-submit-buttons" to='/invitations'>
+                                <div className="plan-submit-buttons">
                                     <div onClick={(event) => {this.props.savePicnik(this.props.planning.recipes, this.props.planning.beers, this.props.planning.wines, this.props.planning.park, this.props.planning.date_of, this.props.planning.time_of, this.props.login);}}>Save Your Picnik and Invite Your Friends</div>
-                                </Link>
+                                </div>
                             </div>
                         </div>
                 </div>
